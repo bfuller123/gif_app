6f6b0f6af683dbec03c8b0c5710a4802b5a79812
Brett Fuller <bfuller123@gmail.com>
2017-05-27 01:51:35 -0500

    edited transition to slowly fadein new element

--- scripts/edit_style.js
+++ scripts/edit_style.js
@@ -1,10 +1,18 @@
 $(document).on('click', '.btn', moveBrand);
 
 function moveBrand(){
+  var button = $('#begin')
+  button.css('opacity', '0');
+  button.attr('disabled', 'true');
   $('.brand').css('width', '25%');
   $('.brand').css('margin', '0 0 0 -1%');
   $('.brand').css('border-radius', '0');
-  $('.brand').html('<h1><span class="underline">Jiffy Giphy</span></h1>');
   $('h1').css('font-size', '42px');
-  $('.jumbotron').css('padding-top', '-36px !important');
+  $('.brand').css('padding-top', '.8%');
+  setTimeout(changeButtonArea, 500);
+}
+
+function changeButtonArea(){
+  $('.buttonArea').html('<input class="addButton"></input><button class="btn btn-primary addButton">Add</button>');
+  $('.addButton').css('animation', 'fadeIn 2s');
 }
